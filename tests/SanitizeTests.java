55ae4699f2cae00f36514311d5affa0d39d70b9c
Joe Morris <joe.morris.dev@gmail.com>
2017-11-01 12:22:24 +0100

    added loading tests

--- tests/SanitizeTests.java
+++ tests/SanitizeTests.java
@@ -1,13 +1,26 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import utils.Sanitize;
+import utils.Sanitize.*;
+
+import java.util.ArrayList;
+
 public class SanitizeTests {
 
     @Test
-    public void LoadForbiddenArray(){}
+    public void LoadForbiddenArray(){
+        String forbiddenCharacters = "#$%";
+        ArrayList<String> forbiddenArray = new ArrayList<>();
+        Assert.assertEquals(3, Sanitize.loadForbiddenArray(forbiddenArray, forbiddenCharacters).size());
+    }
 
     @Test
-    public void LoadPasswordArray(){}
+    public void LoadPasswordArray(){
+        String password = "abc";
+        ArrayList<String> passwordArray = new ArrayList<>();
+        Assert.assertEquals(3, Sanitize.loadPasswordArray(passwordArray, password).size());
+    }
 
     @Test
     public void MarkSingleForbiddenCharacter(){}
