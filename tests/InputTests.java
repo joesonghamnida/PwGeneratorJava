28e05957839fbc8287061253204e8e5adb6c4f58
Joe Morris <joe.morris.dev@gmail.com>
2017-11-12 17:57:12 +0100

    tests for new validation working

--- tests/InputTests.java
+++ tests/InputTests.java
@@ -1,10 +1,21 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import utils.Input;
+
 public class InputTests {
 
     @Test
     public void CheckInputForValidity(){
+        String input = "1234567890";
+
+        Assert.assertTrue(Input.Verify(input));
+
+        input = "123$567890";
+        Assert.assertFalse(Input.Verify(input));
+
+        input = "!234$%as";
+        Assert.assertFalse(Input.Verify(input));
 
     }
 }
